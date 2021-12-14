fef4062c10b58917d6b5fef4ea56af12dfd21b47
Kacper <kacpx123@gmail.com>
2021-12-14 21:30:15 +0100

    fix error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,8 +14,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         window.dataLayer = window.dataLayer || [];
         function gtag() { dataLayer.push(arguments); }
         gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');');
-        `,
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `
      }}
     />
     <Component {...pageProps} />
