6122e68e6213f7fd6596964e7d7dec84474397e4
Kacper <kacpx123@gmail.com>
2021-12-14 21:04:45 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,14 +13,12 @@ const Home: NextPage = () => {
         <a href="https://github.com/latachz" target="_blank" rel="noreferrer">Github</a>
         <a href="https://appunite.com/blog/author/kacper-latuszewski" target="_blank" rel="noreferrer">Blog</a>
       </nav>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
         <span className={styles.backgroundLogo}>{'|>'}</span>Latuszewski
         </h1>
         <p className={styles.description}>Software Engineer at <a href="https://appunite.com/" target="_blank" rel="noreferrer">@appunite</a> and Elixir zealot.</p>
       </main>
-
     </div>
   )
 }
